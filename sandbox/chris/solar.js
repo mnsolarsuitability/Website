e9673e884dc61b692108112a318a88ebafc82ac3
cmartin616 <cmartin616@gmail.com>
2014-10-26 23:15:34 -0500

    Adjusted app button layout.  Added accordian handling to JS.

--- sandbox/chris/solar.js
+++ sandbox/chris/solar.js
@@ -82,7 +82,21 @@ $(window).load(function () {
         var footerHeight = $('#bottomBar').height();
         var offset = parseInt(windHeight) - parseInt(footerHeight);
         $('#bottomBar').css('top',offset);
-    }
+    };
+	
+	// Collapse accordion every time dropdown is shown
+    $('.dropdown-accordion').on('show.bs.dropdown', function (event) {
+      var accordion = $(this).find($(this).data('accordion'));
+      accordion.find('.panel-collapse.in').collapse('hide');
+      });
+
+    // Prevent dropdown to be closed when we click on an accordion link
+    $('.dropdown-accordion').on('click', 'a[data-toggle="collapse"]', function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      $($(this).data('parent')).find('.panel-collapse.in').collapse('hide');
+      $($(this).attr('href')).collapse('show');
+      });
 	
 	function buttonClassRemove(){
 	    $("#solarButton").removeClass("activeButton");
